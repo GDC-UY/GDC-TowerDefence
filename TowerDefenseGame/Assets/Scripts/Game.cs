fcc89a50bfaa558774a04cff8241ff6f9e1adf1e
PedroScheeffer <pedro.scheeffer@correo.ucu.edu.uy>
2023-08-22 11:31:51 -0300

    Cambios Click

---
+++ TowerDefenseGame/Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game instance;
+
+    public static Game Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<Game>();
+                DontDestroyOnLoad(instance.gameObject);
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Rest of the Game class implementation
+}
