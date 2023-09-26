efd7cea0c09b760c5025c8c24afabefd987591ad
JuanPalbo <juan.cerizola@correo.ucu.edu.uy>
2023-09-26 18:15:49 -0300

    Juan Pablo Y León

--- Assets/Scripts/WallBreaker.cs
+++ Assets/Scripts/WallBreaker.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Scenes;
 using UnityEngine;
+using Random = System.Random;
 
-public class WallBreaker : Enemy
+public class WallBreaker: MonoBehaviour
 {
     public int wallBreakPower = 1;
     public float wallBreakTriggerDistance = 5f; //La distancia del muro a la que explota
@@ -10,26 +12,92 @@ public class WallBreaker : Enemy
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Implement logic to select a random wall to break and break it
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // Checkea si esta a la distancia necesaria del muro
-        //if (Vector3.Distance(transform.position, targetPosition) <= wallBreakTriggerDistance)
-        //{
-            BreakWall();
-        //}
+        
+        if (Input.GetKeyDown(KeyCode.B)) 
+        {
+            Debug.Log("tecla peruana apretada");
+            pelela();
+        }
     }
 
     private void BreakWall()
     {
-        // Implement logic to select a random wall to break and break it
-        // You might need a separate script to manage walls and their health
+        // Implement logic to select random walls to break and break it
+        //Generate random number between 3 and 5
+        GridManager gm = GridManager.Instance;
+        int breakRand = UnityEngine.Random.Range(3, 6);
+        
+        int contador = 0;
+        bool check = false;
+        Node nodito = null;
+        while (contador < breakRand)
+        {
+            while (check != true)
+            {
+                int randomX = UnityEngine.Random.Range(0, gm.Width);
+                int randomY = UnityEngine.Random.Range(0, gm.Height);
+                nodito = gm.nodes[randomX, randomY];
+                if (nodito.GetUsed() && !nodito.GetCell().HasAttachedTurret())
+                {
+                    check = true;
+                }
+            }
+            LinkedList<Node> adyacentes = nodito.GetAdy();
+            List<Node> nodosARomper = new List<Node>();
+            var adyacente = adyacentes.First;
+            while (contador <= breakRand)
+            {
+                if (adyacente.Value.GetUsed() && !adyacente.Value.GetCell().HasAttachedTurret())
+                {
+                    adyacente.Value.SetUsed(false);
+                    foreach (Node ady in adyacente.Value.GetAdy())
+                    {
+                        if (ady.GetUsed())
+                        {
+                            nodosARomper.Add(ady);
+                        }
+                    }
+                    contador++;
+                }
+                adyacente = adyacente.Next;
+                if (adyacente == null)
+                {
+                    break;
+                }
+            }
+            if(contador < breakRand)
+            {
+                while(contador <= breakRand)
+                {
+                    int random = UnityEngine.Random.Range(0, (nodosARomper.Count)-1);
+                    if (nodosARomper[random].GetValue() && !nodosARomper[random].GetCell().HasAttachedTurret())
+                    {
+                        nodosARomper[random].SetUsed(false);
+                        contador++;
+                    }
+                }
+            }
+        }
+
+        gm.previewPath();
+
+    }
+
+    private void pelela()
+    {
+        LinkedList<Node> muritos = GridManager.Instance.graph.GetFreeWalls();
+        if (muritos.Count > 5)
+        {
+            Debug.Log("Break Wall");
+            BreakWall();
+        }
     }
 
 
-
-    
 }
